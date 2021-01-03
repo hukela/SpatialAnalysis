483aac7bce3402a8039581a30ef01e8938da4ef0
达达_胡克拉 <hiv_h1@163.com>
2021-01-03 13:56:16 +0800

    升级为管理员权限

---
+++ SpatialAnalysis/Entity/Incident.cs
@@ -0,0 +1,16 @@
+﻿namespace SpatialAnalysis.Entity
+{
+    class Incident
+    {
+        //事件标题
+        public string title;
+        //事件备注
+        public string comment;
+        //事件类型
+        public IncideatType type;
+    }
+    enum IncideatType
+    {
+        daily, install, clear
+    }
+}
---
+++ SpatialAnalysis/IO/XML.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;
+
+namespace SpatialAnalysis.IO
+{
+    class XML
+    {
+        //存储文件
+        private const string filePath = "/Data/Core.xml";
+        private static readonly XElement xmlDoc = XElement.Load(filePath);
+    }
+}
